57d6e02ba6e5cfe3a28835434a222b55c4b55ef8
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2021-04-19 15:46:07 +0200

    Navigating to the "Filtered Events" Page Progammatically

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,19 @@
 import { getAllEvents } from "../../dummy_data";
+import { useRouter } from 'next/router'
 import EventList from "../../components/events/event-list";
 import EventSearch from "../../Components/events/events-search";
 
 function Events(){
     const events = getAllEvents()
+    const router = useRouter()
 
+    function onSearch(year, month){
+        const fullPath = `/events/${year}/${month}`
+        router.push(fullPath)
+    }
     return (
         <div>
-            <EventSearch />
+            <EventSearch onSearch={onSearch}/>
             <EventList items={events} /> 
         </div>
     )
